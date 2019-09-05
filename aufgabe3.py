422a8ecf8ed6777ad10b8c6cc78ba6b9997f5b5b
Lena He <deguomumu@gmail.com>
2019-09-05 22:19:14 +0200

    add etwas

--- aufgabe3.py
+++ aufgabe3.py
@@ -1,10 +1,14 @@
 password = 'a123456'
-n = 1
-while n <= 3:
+n = 3
+while n > 0:
+	n = n - 1
 	pwd = input('请输入密码:')
 	if pwd == password:
 		print('登录成功！')
 		break
 	else:
-		print('密码错误！ 还有', 3-n, '次机会')
-		n = n + 1
+		print('密码错误! ')
+		if n > 0:
+			print('还有', n, '次机会')
+		else:
+			print('没有机会了 账号已锁')
